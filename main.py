7b177572573e5276b0887fbea7bfaf26e650343c
mikey241124 <48805730+mikey241124@users.noreply.github.com>
2024-04-25 13:18:02 -0400

    Made basic gui outline, main menu with buttons
    Still need to make buttons interactive

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-import random
-from tkinter import Label, Tk, Canvas, ALL
+import sys
+import pygame as pygame
 
 
 class Main:
-    if __name__ == '__main__':
-        # gamestate 0, 1, 2 are main menu, game menu, end screen
-        gameState = 0
+    WIDTH = 1200
+    HEIGHT = 800
+
+    def draw_game_start(screen):
+        WIDTH = 1200
+        HEIGHT = 800
+
+        BLACK = (0, 0, 0)
+        WHITE = (255, 255, 255)
+        RED = (50, 50, 50)
+        CREAM = (255, 253, 208)
+
+        title_font = pygame.font.Font(None, 150)
+        button_font = pygame.font.Font(None, 90)
+
+        screen.fill(CREAM)
+
+        title_surface = title_font.render("Sudoku", 0, BLACK)
+        title_rectangle = title_surface.get_rect(center=(WIDTH//2, HEIGHT//2 - 250))
+        screen.blit(title_surface, title_rectangle)
+
+        easy_surface = button_font.render("Easy", 0, BLACK)
+        easy_rectangle = easy_surface.get_rect(center=(WIDTH//2, HEIGHT//2 - 40))
+        screen.blit(easy_surface, easy_rectangle)
+
+        medium_surface = button_font.render("Medium", 0, BLACK)
+        medium_rectangle = medium_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 70))
+        screen.blit(medium_surface, medium_rectangle)
+
+        hard_surface = button_font.render("Hard", 0, BLACK)
+        hard_rectangle = hard_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 180))
+        screen.blit(hard_surface, hard_rectangle)
+
+
 
         while True:
-            if gameState == 0:
-                pass
-            elif gameState == 1:
-                pass
-            elif gameState == 2:
-                pass
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if start_rectangle.collidepoint(event.pos):
+                        return
+                #elif quit_rectangle.collidepoint(event.pos):
+                #    sys.exit()
+            pygame.display.update()
+
+
+    if __name__ == '__main__':
+        game_over = False
+
+        pygame.init()
+        screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Sudoku")
+
+        draw_game_start(screen)
