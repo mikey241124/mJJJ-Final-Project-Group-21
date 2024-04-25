48fd0e607acb389ac13b12a2dc8fb4c31d46d0af
mikey241124 <48805730+mikey241124@users.noreply.github.com>
2024-04-25 11:10:19 -0400

    Removed code from main
    Method wasn't working, have to think of something else for GUI

--- main.py
+++ main.py
@@ -7,30 +7,6 @@ class Main:
         # gamestate 0, 1, 2 are main menu, game menu, end screen
         gameState = 0
 
-        gameWidth = 1000
-        gameHeight = 800
-        window = Tk()
-        window.title('Sudoku Game')
-        window.resizable(False, False)
-        canvas = Canvas(window, bg='#000000', height=gameHeight, width=gameWidth)
-        canvas.pack
-
-        window.update()
-        window_width = window.winfo_width()
-        window_height = window.winfo_height()
-        screen_width = window.winfo_screenwidth()
-        screen_height = window.winfo_screenheight()
-
-        x = int((screen_width / 2) - (window_width / 2))
-        y = int((screen_height / 2) - (window_height / 2))
-
-        window.geometry(f"{window_width}x{window_height}+{x}+{y}")
-
-#        window.bind('<Left>', lambda event: change_direction('left'))
-#        window.bind('<Up>', lambda event: change_direction('up'))
-#        window.bind('<Right>', lambda event: change_direction('right'))
-#        window.bind('<Down>', lambda event: change_direction('down'))
-
         while True:
             if gameState == 0:
                 pass
